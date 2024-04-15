34d2647872e0d9b21220bf775ef664f5a5a54d47
jhansi cheruku <chjhansi97@gmail.com>
2024-04-15 11:20:08 +0200

    add models

--- app/models.py
+++ app/models.py
@@ -13,21 +13,30 @@ class Target(db.Model):
     def __repr__(self):
         return self.title
 
+class Crawler(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    type = db.Column(db.String(128))
+    cmd = db.Column(db.String(128), index=True)
+    settings = db.Column(db.Text, index=True)
+
+    def __repr__(self):
+        return self.id
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    storagepath = db.Column(db.String(100))
-    resultOutcome = db.Column(db.String(100))
+    storage_path = db.Column(db.String(100))
+    result_outcome = db.Column(db.String(100))
     stats = db.Column(db.String(100))
     QA = db.Column(db.String(100))
     schedule = db.Column(db.String(100))
-    startDateTime = db.Column(db.DateTime)
-    EndDateTime = db.Column(db.DateTime)
+    start_date_time = db.Column(db.DateTime)
+    end_date_time = db.Column(db.DateTime)
 
     target_id = db.Column(db.Integer, db.ForeignKey('target.id'))
     target = db.relationship('Targets', backref=db.backref('jobs', lazy=True))
     crawler_id = db.Column(db.Integer, db.ForeignKey('crawler.id'))
-    crawler = db.relationship('Crawler', backref=db.backref('jobs', lazy=True))
+    crawler = db.relationship('Crawlers', backref=db.backref('jobs', lazy=True))
     # software
     # settings
     result = db.Column(db.String(128), index=True)
@@ -35,12 +44,15 @@ class Job(db.Model):
     def __repr__(self):
         return self.id
     
-class ContentOwners():
+class ContentOwners(db.Model):
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(128), index=True)
     reference_code = db.Column(db.String(128), index=True)
 
-class Seeds():
+    def __repr__(self):
+        return self.id
+
+class Seeds(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(128), index=True)
     depth = db.Column(db.Integer, index=True)
@@ -48,19 +60,20 @@ class Seeds():
     include_patterns = db.Column(db.String(128), index=True)
     domains = db.Column(db.String(128), index=True)
 
-    target_id = db.Column(db.Integer, db.ForeignKey('targets.id'))
-    target = db.relationship('Targets', backref=db.backref('seeds', lazy=True))
-    
-class Users():
+    target_id = db.Column(db.Integer, db.ForeignKey('target.id'))
+    target = db.relationship('Target', backref=db.backref('seeds', lazy=True))
+
+    def __repr__(self):
+        return self.id
+
+class Users(db.Model):
     id = db.Column(db.String,primary_key=True, default=lambda: str(uuid.uuid4()))
     first_name = db.Column(db.String(128), index=True)
     last_name = db.Column(db.String(128), index=True)
     email = db.Column(db.String(128), index=True)
     password = db.Column(db.String(128), index=True)
 
-class Crawler():
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), index=True)
-    type = db.Column(db.String(128))
-    cmd = db.Column(db.String(128), index=True)
-    settings = db.Column(db.Text, index=True)
+    def __repr__(self):
+        return self.id
+
+
