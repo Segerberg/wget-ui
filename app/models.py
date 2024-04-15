6ac1f73e7aea9e8c758125de66a3e1daaab09172
jhansi cheruku <chjhansi97@gmail.com>
2024-04-15 13:27:46 +0200

    latest

--- app/models.py
+++ app/models.py
@@ -26,8 +26,7 @@ class User(UserMixin, db.Model):
 
 
 class Target(db.Model):
-    #id = db.Column(db.String,primary_key=True, default=lambda: str(uuid.uuid4()))
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.String,primary_key=True, default=lambda: str(uuid.uuid4()))
     title = db.Column(db.String(128), index=True)
     description = db.Column(db.String(128), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
